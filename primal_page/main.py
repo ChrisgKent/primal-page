4cbcf95aabb43cc664c483264c8944635ca872b9
ChrisKent <chrisgkent@gmail.com>
2024-02-07 11:14:15 +0000

    Made the config.json optional.

--- primal_page/main.py
+++ primal_page/main.py
@@ -5,6 +5,7 @@ import shutil
 import hashlib
 import json
 from typing import Optional
+from enum import Enum
 
 from primal_page.build_index import create_index
 from primal_page.schemas import (
@@ -16,6 +17,11 @@ from primal_page.schemas import (
     BedfileVersion,
     PrimerNameVersion,
 )
+
+
+class FindResult(Enum):
+    NOT_FOUND = 1
+    FOUND = 2
 
 
 # Create the typer app
@@ -189,14 +195,13 @@ def find_config(
     cli_config: pathlib.Path | None,
     found_files: list[pathlib.Path],
     schemepath: pathlib.Path,
-) -> pathlib.Path:
+) -> tuple[FindResult, pathlib.Path | None]:
     """
     Find the config.json file
     :param cli_config: The config.json file specified by the user. None if not specified
     :param found_files: A list of all files found in the scheme directory
     :param schemepath: The path to the scheme directory
     :return: The path to the config.json file
-    :raises FileNotFoundError: If the config.json file cannot be found
     """
     # Search for config.json
     if cli_config is None:  # No config.json specified
@@ -204,17 +209,17 @@ def find_config(
         config_list: list[pathlib.Path] = [
             path for path in found_files if path.name == ("config.json")
         ]
-        if len(config_list) == 1:
-            return config_list[0]
-        else:
-            raise FileNotFoundError(
-                f"Could not find a SINGLE config.json file in {schemepath} or its subdirectories, found {len(config_list)}. Please specify manually with --config"
-            )
+        match len(config_list):
+            case 1:
+                return (FindResult.FOUND, config_list[0])
+            case _:
+                return (FindResult.NOT_FOUND, None)
+
     elif cli_config.exists():
         # TODO validate the config.json file
-        return cli_config
+        return (FindResult.FOUND, cli_config)
     else:
-        raise FileNotFoundError(f"Could not find file at {cli_config}")
+        return (FindResult.NOT_FOUND, None)
 
 
 @app.command()
@@ -310,24 +315,34 @@ def create(
     valid_ref = find_ref(reference, found_files, schemepath)
 
     # Search for config.json
-    configpath = find_config(configpath, found_files, schemepath)
-    # Read in the config
-    config_json: dict = json.load(configpath.open())
-    # parse the config.json to get algorithm
-    if algorithmversion is None:
-        if "algorithmversion" in config_json.keys():
-            algorithmversion = str(config_json["algorithmversion"])
-        else:
-            raise ValueError(
-                f"Could not find algorithmversion in config.json, please specify manually with --algorithmversion, in form of 'algorithm':'version'"
-            )
+    result = find_config(configpath, found_files, schemepath)
+    config_json: None | dict = None  # type: ignore
+    if (
+        result[0] == FindResult.FOUND and result[1] is not None
+    ):  # Second check is for mypy
+        configpath = result[1]
+        # Read in the config
+        config_json: dict = json.load(configpath.open())
+        # Remove some paths from the config
+        config_json.pop("output_dir", None)
+        # These hashes are no longer used, so remove to prevent confusion
+        md5s_keys_to_remove = {k for k in config_json.keys() if k.endswith("md5")}
+        for k in md5s_keys_to_remove:
+            config_json.pop(k)
 
-    # Remove some paths from the config
-    config_json.pop("output_dir", None)
-    # These hashes are no longer used, so remove to prevent confusion
-    md5s_keys_to_remove = {k for k in config_json.keys() if k.endswith("md5")}
-    for k in md5s_keys_to_remove:
-        config_json.pop(k)
+        if algorithmversion is None:
+            if "algorithmversion" in config_json.keys():
+                algorithmversion = str(config_json["algorithmversion"])
+            else:
+                raise ValueError(
+                    f"algorithmversion not specified in {configpath}. Please specify manually with --algorithmversion"
+                )
+
+    elif result[0] == FindResult.NOT_FOUND:
+        if algorithmversion is None:
+            raise FileNotFoundError(
+                f"Could not find a config.json file in {schemepath}. Please specify manually with --configpath or specify algorithmversion with --algorithmversion"
+            )
 
     # Multiple pngs/htmls/msas are allowed
     # The single check is mainly to prevent multiple schemes via providing the wrong directory
@@ -371,7 +386,7 @@ def create(
         status=schemestatus,
         citations=set(citations),
         authors=set(authors),
-        algorithmversion=algorithmversion,
+        algorithmversion=algorithmversion,  # type: ignore
         species=set(species),
         description=description,
         derivedfrom=derivedfrom,
@@ -404,9 +419,10 @@ def create(
         working_dir = repo_dir / "work"
         working_dir.mkdir()
 
-        # Write out the config.json
-        with open(working_dir / "config.json", "w") as configfile:
-            json.dump(config_json, configfile, indent=4, sort_keys=True)
+        if config_json is not None:
+            # Write out the config.json
+            with open(working_dir / "config.json", "w") as configfile:
+                json.dump(config_json, configfile, indent=4, sort_keys=True)
 
         # Copy over misc files
         for misc_file in misc_files_to_copy:
