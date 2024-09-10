160952c5f6c8128e2a5e1bb966b6f63da1256d8f
ChrisKent <chrisgkent@gmail.com>
2024-09-10 15:34:58 +0100

    Updated test outputs

--- tests/create/test_create.py
+++ tests/create/test_create.py
@@ -3,8 +3,7 @@ import pathlib
 import shutil
 import unittest
 
-from primal_page.errors import FileNotFound
-from primal_page.main import FindResult, create, find_config, find_primerbed, find_ref
+from primal_page.main import create
 from primal_page.schemas import SchemeStatus
 
 
@@ -40,7 +39,6 @@ class TestCreate(unittest.TestCase):
 
         # Run the create function
         create(
-            schemepath=self.schemepath,
             output=self.output,
             ampliconsize=self.ampliconsize,
             schemeversion=self.schemeversion,
@@ -49,7 +47,9 @@ class TestCreate(unittest.TestCase):
             citations=self.citations,
             authors=self.authors,
             schemename=self.schemename,
-            reference=None,
+            reference=self.reference,
+            primerbed=self.primerbed,
+            algorithmversion=self.algorithmversion,
         )
 
         # Check the output files exist
@@ -96,128 +96,10 @@ class TestCreate(unittest.TestCase):
 
         # Test the non provided fields are correct
         self.assertEqual(
-            info["algorithmversion"], "primalscheme3:1.0.0"
+            info["algorithmversion"], self.algorithmversion
         )  # parsed from config
         self.assertEqual(info["description"], None)
         self.assertEqual(info["derivedfrom"], None)
-
-
-class Test_Find(unittest.TestCase):
-    def setUp(self) -> None:
-        self.schemepath = pathlib.Path("tests/test_input/test_covid")
-        self.found_files = [x for x in self.schemepath.rglob("*")]
-
-    def test_find_ref(self):
-        """
-        Test the find_reference function can find a single reference file
-        """
-        result = find_ref(
-            cli_reference=None, found_files=self.found_files, schemepath=self.schemepath
-        )
-        # See if it can find the reference file
-        self.assertEqual(
-            result, pathlib.Path("tests/test_input/test_covid/reference.fasta")
-        )
-
-        # See if it can find the given reference file
-        resultcli = find_ref(
-            cli_reference=pathlib.Path(
-                "tests/test_input/test_covid/primer.bed"
-            ),  # Provide a different file # Might fail in future when ref is validated
-            found_files=self.found_files,
-            schemepath=self.schemepath,
-        )
-        self.assertEqual(
-            resultcli, pathlib.Path("tests/test_input/test_covid/primer.bed")
-        )
-
-        # Test fail when given a file with two refs
-        with self.assertRaises(FileNotFound):
-            new_schemepath = pathlib.Path(
-                "tests/test_input"
-            )  # This dir contains two schemes dirs
-            new_found_files = [x for x in new_schemepath.rglob("*")]
-            find_ref(None, new_found_files, new_schemepath)
-
-        # Test fail when given a file that doesn't exist
-        with self.assertRaises(FileNotFound):
-            find_ref(
-                cli_reference=pathlib.Path(
-                    "tests/test_input/test_covid/missingref.fasta"
-                ),
-                found_files=self.found_files,
-                schemepath=self.schemepath,
-            )
-
-    def test_find_primerbed(self):
-        """
-        Test the find_primerbed function can find a single primerbed file
-        """
-        result = find_primerbed(
-            cli_primerbed=None,
-            found_files=self.found_files,
-            schemepath=self.schemepath,
-        )
-        # See if it can find the primerbed file
-        self.assertEqual(result, pathlib.Path("tests/test_input/test_covid/primer.bed"))
-
-        # See if it can find the given primerbed file
-        resultcli = find_primerbed(
-            cli_primerbed=pathlib.Path(
-                "tests/test_input/test_covid/primer.bed"
-            ),  # Provide a different file # Might fail in future when ref is validated
-            found_files=self.found_files,
-            schemepath=self.schemepath,
-        )
-        self.assertEqual(
-            resultcli, pathlib.Path("tests/test_input/test_covid/primer.bed")
-        )
-
-        # Test fail when given a file with two refs
-        with self.assertRaises(FileNotFound):
-            new_schemepath = pathlib.Path("tests/test_input")
-            new_found_files = [x for x in new_schemepath.rglob("*")]
-            find_primerbed(None, new_found_files, new_schemepath)
-
-        # Test fail when given a file that doesn't exist
-        with self.assertRaises(FileNotFound):
-            find_primerbed(
-                cli_primerbed=pathlib.Path("tests/test_input/test_covid/missing.bed"),
-                found_files=self.found_files,
-                schemepath=self.schemepath,
-            )
-
-    def test_find_config(self):
-        """
-        Test the find_config function can find a single config file
-        """
-        result, path = find_config(
-            cli_config=None,
-            found_files=self.found_files,
-            schemepath=self.schemepath,
-        )
-        # See if it can find the config file
-        self.assertEqual(path, pathlib.Path("tests/test_input/test_covid/config.json"))
-
-        # See if it can find the given config file
-        result, path = find_config(
-            cli_config=pathlib.Path(
-                "tests/test_input/test_covid/config.json"
-            ),  # Provide a different file # Might fail in future when ref is validated
-            found_files=self.found_files,
-            schemepath=self.schemepath,
-        )
-        self.assertEqual(path, pathlib.Path("tests/test_input/test_covid/config.json"))
-
-        # Test fail when given a file with two refs
-        new_schemepath = pathlib.Path("tests/test_input")
-        new_found_files = [x for x in new_schemepath.rglob("*")]
-        result, path = find_config(None, new_found_files, new_schemepath)
-        self.assertEqual(result, FindResult.NOT_FOUND)
-
-        # Test fail when given a file that doesn't exist
-        result, path = find_config(None, new_found_files, new_schemepath)
-        self.assertEqual(result, FindResult.NOT_FOUND)
 
 
 if __name__ == "__main__":
